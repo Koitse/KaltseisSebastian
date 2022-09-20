1bf759c28203e835d370bdbf89ec988d45ee935e
Koitse <kaltseisS190203@sus.htl-grieskirchen.at>
2022-09-20 16:24:09 +0200

    Zuhause weiter arbeiten

--- src/net/htlgrieskirchen/pos2/kaltseisSebastian/EratosthenesPrimeSieve.java
+++ src/net/htlgrieskirchen/pos2/kaltseisSebastian/EratosthenesPrimeSieve.java
@@ -2,56 +2,59 @@ package net.htlgrieskirchen.pos2.kaltseisSebastian;
 
 import java.awt.*;
 import java.lang.reflect.Array;
+import java.math.MathContext;
 import java.util.ArrayList;
+import java.util.List;
 
 public class EratosthenesPrimeSieve implements PrimeSieve {
 
-
     int menge;
-    ArrayList prime = new ArrayList();
+    private List<Integer> prime = new ArrayList();
     private int counter = 0;
 
     public EratosthenesPrimeSieve(int menge) {
-        this.menge = menge;
+        prime.add(menge);
     }
 
     @Override
     public boolean isPrime(int p) {
 
-        if(p==0||p==1)
-        {
-            return false;
+        boolean isprime;
+        for (int i = p; i < prime.size() ; i++) {
+
+            if (p <= 0) {
+                isprime = false;
+            } else {
+                if (p == 1 || p == 2) {
+                    isprime = true;
+
+
+                } else {
+                    isprime = true;
+
+                    for (int x = 2; x < p; x++) {
+                        if (p % x == 0) {
+                            isprime = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (isprime) {
+                System.out.println(p + " " + "Primzahl");
+            } else {
+                System.out.println(p + " " + "keine Primzahl");
+            }
         }
-
-        if(p==2||p==3||p==5)
-        {
-            prime.add(p);
-            return false;
-        }
-
-        if(p%2 == 0)
-        {
-            return false;
-        }
-
-        if(p%3 == 0)
-        {
-            return false;
-        }
-
-        if(p%5 == 0)
-        {
-            return false;
-        }
-
-
-        return true;
+      return true;
     }
 
     @Override
     public void printPrimes() {
-        for (int i = 0; i < prime.size(); i++) {
-            System.out.println(prime.get(i));
+        for (Integer o:prime) {
+            System.out.println(o);
+
         }
     }
 }
